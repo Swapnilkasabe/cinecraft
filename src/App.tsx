52eae85476c932937d6c769f0318282ae6f07997
Swapnil <swapnilkasabe02@gmail.com>
2024-12-05 23:12:00 +0530

    Integrate movie data and define MovieDataType structure with context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { MovieProvider } from './context/movie-context'
 
 const App = () => {
   return (
+    <MovieProvider>
     <RouterProvider router={router}/>
+    </MovieProvider>
   )
 }
 
